979c05690937ced49e954f50fc74a53a0ac66b12
onscreen <onscreen@onscreenplayer.wizai.com.local.lan>
2018-07-11 16:28:52 +0200

    Dynamische erzeugung eines Objekt der anhand von daten die playlist dynamisch erzeugt. Eine funktion/ Ebenso eine 2. Variante für 2 Funktionen? best case?

--- playlist/src/assets/js/test.js
+++ playlist/src/assets/js/test.js
@@ -6,7 +6,7 @@ var rawItems = [
 	{ name: 'bild3', type: 0 }, 
 	{ name: 'bild4', type: 0 },
 	{ name: 'bild5', type: 0 },
-	{ name: 'bild6', type: 1 }, 
+	{ name: 'bild6', type: 0 }, 
 	{ name: 'bild1', type: 1 }, 
 	{ name: 'bild2', type: 1 },
 	{ name: 'bild3', type: 1 }, 
@@ -15,74 +15,128 @@ var rawItems = [
 	{ name: 'bild6', type: 3 },
 	{ name: 'bild1', type: 3 },
 	{ name: 'bild2', type: 3 }, 
-	{ name: 'bild3', type: 5 },
+	{ name: 'bild3', type: 4 },
+	{ name: 'bild3', type: 4 },
 	{ name: 'bild4', type: 5 },
 	{ name: 'bild5', type: 5 }, 
 	{ name: 'bild6', type: 5 },
 	{ name: 'bild1', type: 5 }, 
+	{ name: 'bild4', type: 5 }, 
 ];
 
-var obj = { 0: [] };
 var playList = [];
-var element = 0,
-	count = 0;
+
+
 
 function init() {
 	
 	try {
-		setObjectContent(rawItems);
-		
+//			generatePlayList(playList);
+		generatePlayList(setObjectContent, playList);
+		console.log(playList)
+
 		try {
-			generatePlayList(playList);
-			
-			try {
-				var playInterval = setInterval(play_playList, 2000);
-				
-			} catch (err) {
-				clearInterval(playInterval);
-				console.log(err);
-			}
-			
+			var playInterval = setInterval(play_playList, 2000);
+
 		} catch (err) {
+			clearInterval(playInterval);
 			console.log(err);
 		}
-		
+
 	} catch (err) {
 		console.log(err);
-	}	
+	}
 }
 
-function setObjectContent(items) {
+
+
+
+//adding the non priority propertys in the obj{ 0:[] } by the values type;
+function setObjectContent() {
+	var obj = new Object();
 	
-	$.each(items, function(key,value) {
-		if (value.type < 2) {
-			obj[0].push(items[key])
-		} else if (typeof obj[key] === 'undefined' && typeof obj[value.type] == 'undefined') {
-			obj[value.type] = [];
-			obj[value.type].push(value);
+	$.each(rawItems, function(key, value) {
+		
+		if (value.type < 2 || value.type % 4 === 0) {
+			
+			if (Object.keys(obj).length === 0) {
+				obj[0] = new Array(value);	
+			} else {
+				obj[0].push(value);
+			}
+			
+		} else if (typeof obj[key] === 'undefined' && typeof obj[value.type] === 'undefined') {
+			obj[value.type] = new Array(value);
 		} else {
 			obj[value.type].push(value);
 		}
+		
 	});
+	return obj;
 }
-
-function generatePlayList(list) {
+//Fills the playlist with the values that exist on the vouchenen propertys
+function generatePlayList(setObj, list) {
 	
+	var obj = setObj();
 	var index;
-	$.each(obj[0], function(key,value) {		
+	
+	$.each(obj[0], function(key, value) {
 		index = 0;
-		$.each(obj[index], function(k,val) {	
-			if (key % k == 0 && typeof obj[index] != 'undefined') {
+		$.each(obj[index], function(k, val) {	
+			if (key % k === 0 && typeof obj[index] !== 'undefined') {
 				list.push(obj[index]);
 			}
 			index++;
 		});
 		list.push(value)
 	});
-	console.log(list)
 }
 
+/*
+function generatePlayList(items, list) {
+	
+	var obj = new Object()
+	var index;
+	
+	//adding the non priority propertys in the obj{ 0:[] } by the values type;
+	$.each(items, function(key, value) {
+		if (value.type < 2 || value.type % 4 === 0) {
+			if (Object.keys(obj).length === 0) {
+				obj[0] = new Array(value);
+			} else {
+				obj[0].push(value);
+			}
+			
+		} else if (typeof obj[key] === 'undefined' && typeof obj[value.type] === 'undefined') {
+			obj[value.type] = new Array(value);
+		} else {
+			obj[value.type].push(value);
+		}
+	});
+	
+	//Fills the playlist with the values that exist on the vouchenen propertys
+	$.each(obj[0], function(key, value) {	
+		index = 0;
+		$.each(obj[index], function(k, val) {
+			if (key % k === 0 && typeof obj[index] !== 'undefined') {
+				list.push(obj[index]);
+			}
+			index++;
+		});
+		list.push(value)
+	});
+}
+*/
+
+
+
+
+
+
+
 // OPTIONAL _ JUST FOR OUTPUT
+var element = 0,
+	count = 0;
 function play_playList() {
 	
 	var screen = document.getElementById('playList_1');
